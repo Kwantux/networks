694c90cd82698fe7b8f99c125df34929ecd0303b
Quantum625 <linpec@protonmail.com>
2023-06-16 19:15:17 +0200

    Update v2.0.2

--- src/main/java/net/quantum625/networks/listener/AutoSave.java
+++ src/main/java/net/quantum625/networks/listener/AutoSave.java
@@ -4,7 +4,6 @@ import net.quantum625.networks.Main;
 import net.quantum625.networks.NetworkManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.world.WorldLoadEvent;
 import org.bukkit.event.world.WorldSaveEvent;
 
 
