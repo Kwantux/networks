e695606462df32cd7872cc78ce88037200dfa3e0
Quantum625 <linpec@protonmail.com>
2023-08-08 20:44:54 +0200

    Fixed that the notice message came when opening a crafting table

--- src/main/java/net/quantum625/networks/listener/InventoryOpenEventListener.java
+++ src/main/java/net/quantum625/networks/listener/InventoryOpenEventListener.java
@@ -1,15 +1,13 @@
 package net.quantum625.networks.listener;
 
-import net.quantum625.config.lang.Language;
 import net.quantum625.networks.Main;
 import net.quantum625.networks.NetworkManager;
-import net.quantum625.networks.data.Config;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-
+import org.bukkit.event.inventory.InventoryType;
 
 public class InventoryOpenEventListener implements Listener {
     private final NetworkManager net;
@@ -20,7 +18,7 @@ public class InventoryOpenEventListener implements Listener {
 
     @EventHandler(priority= EventPriority.LOW)
     public void onInventoryOpen(InventoryOpenEvent event) {
-        if (event.getInventory().firstEmpty() == -1) {
+        if (event.getInventory().firstEmpty() == -1 && event.getInventory().getType().equals(InventoryType.CHEST)) {
             net.noticePlayer((Player) event.getPlayer());
         }
     }
