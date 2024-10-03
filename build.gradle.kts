8b43edf78d7610b131c188746f74bbddd74fa792
Kwantux <git@kwantux.de>
2024-10-03 15:31:25 +0200

    Lowered Paper API Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 runPaper.folia.registerTask()
 
 group = "de.kwantux"
-version = "3.1.0-SNAPSHOT-a1"
+version = "3.0.4-SNAPSHOT"
 description = "A performance friendly way to sort your items"
 
 repositories {
@@ -21,23 +21,22 @@ repositories {
 }
 
 dependencies {
-    compileOnly("dev.folia", "folia-api", "1.20.6-R0.1-SNAPSHOT")
+    compileOnly("dev.folia", "folia-api", "1.20.1-R0.1-SNAPSHOT")
     paperLibrary("net.kyori", "adventure-text-minimessage", "4.13.1")
     paperLibrary("org.spongepowered", "configurate-hocon", "4.1.2")
     paperLibrary("org.spongepowered", "configurate-yaml", "4.1.2")
-    paperLibrary("org.incendo", "cloud-core", "2.0.0")
     paperLibrary("org.incendo", "cloud-paper", "2.0.0-beta.10")
     paperLibrary("com.google.code.gson", "gson", "2.10.1")
 }
 
 java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 }
 
 paper {
     main = "de.kwantux.networks.Main"
     loader = "de.kwantux.networks.Loader"
-    apiVersion = "1.20.6"
+    apiVersion = "1.20"
     load = BukkitPluginDescription.PluginLoadOrder.STARTUP
     website = "https://github.com/Kwantux/Networks"
     authors = listOf("Kwantux")
